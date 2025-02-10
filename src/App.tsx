cb36bca2ad2114221398dde932484a14add789e3
aaron <xiaofengliu1992@hotmail.com>
2025-02-10 17:14:34 -0500

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to Vite + React</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR (Hot Module
+        Replacement).
+      </p>
+    </div>
+  );
+};
+
+export default App;
