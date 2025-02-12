5fa6d04c605b622dce8a428ea3c659e3a702f6f3
aaron <xiaofengliu1992@hotmail.com>
2025-02-12 10:40:14 -0500

    template for rtk

--- src/redux/reducer/userSlice.ts
+++ src/redux/reducer/userSlice.ts
@@ -13,15 +13,12 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    increment: (state) => {
-      state.email = "";
-    },
-    incrementByAmount: (state, action: PayloadAction<string>) => {
+    updateUser: (state, action: PayloadAction<string>) => {
       state.email = action.payload;
     },
   },
 });
 
-export const { increment, incrementByAmount } = userSlice.actions;
+export const { updateUser } = userSlice.actions;
 export const user = (state: RootState) => state.user.email;
 export default userSlice.reducer;
