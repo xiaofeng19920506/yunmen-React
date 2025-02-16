41a4a4dcb14df0d1a68a1d1754ef04b44671f2ea
aaron <xiaofengliu1992@hotmail.com>
2025-02-15 19:34:09 -0500

    remove strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useAppSelector } from "./hooks/reduxHooks";
+import { RootState } from "./redux/store/store";
+
 const App = () => {
+  const { email } = useAppSelector((state: RootState) => state.user);
+
+  console.log(email);
   return (
     <div className="App">
       <h1>Welcome to Vite + React</h1>
       <p>
-        Edit <code>src/App.tsx</code> and save to test HMR (Hot Module
-        Replacement).
+        Edit <code>{email}</code> and save to test HMR (Hot Module Replacement).
       </p>
     </div>
   );
