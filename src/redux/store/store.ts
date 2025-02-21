608fa2ee43ebe42e6c053b9bd90b1a21e78ade6c
aaron <xiaofengliu1992@hotmail.com>
2025-02-20 19:52:38 -0500

    remove query

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,16 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../reducer/user/userSlice";
 import modalReducer from "../reducer/eventModal/eventModalSlice";
-import { userApi } from "../reducer/user/userApiSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    [userApi.reducerPath]: userApi.reducer,
     modal: modalReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
